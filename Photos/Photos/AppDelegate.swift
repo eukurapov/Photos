a2ebca409bc1bb3a50777acb1e20e0163b424212
eukurapov <eukurapov@gmail.com>
2020-10-12 16:57:27 +0300

    Init commit - project w/o a storyboard

---
+++ Photos/Photos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Photos
+//
+//  Created by Eugene Kurapov on 12.10.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+    
+}
+
